c29df488b1642cd1e60c4b92a07017a2f98b996e
黃意芹 <ctps930080@gmail.com>
2019-05-21 12:20:27 +0800

    hashrouter

--- vocational_workshop/src/containers/root.js
+++ vocational_workshop/src/containers/root.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './header';
 import Main from './main';
 import Footer from './footer/footer';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 class Root extends Component {
   constructor(props) {
     super(props);
@@ -12,13 +12,13 @@ class Root extends Component {
   render() 
     {
       return (
-        <BrowserRouter>
+        <HashRouter>
           <div>
               <Header page = {this.state.page}/>
               <Main/> 
               <Footer/>
           </div>
-        </BrowserRouter>
+        </HashRouter>
       
       )
     }

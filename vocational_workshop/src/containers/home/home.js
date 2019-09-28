27e27be726e792b8394238d23ab5a36d4dd1f2a4
Belong112 <belongbelong223@gmail.com>
2019-09-28 11:49:05 +0800

    homepage_modify

--- vocational_workshop/src/containers/home/home.js
+++ vocational_workshop/src/containers/home/home.js
@@ -41,20 +41,27 @@ class Home extends Component {
                         width={window.innerWidth*0.7} height={window.innerWidth * 0.39}/> 
                     </div>
                 </div>
-                <div className = "text-center text-container">
+                <div className="small-container text-center text-container my-5">
                     <h1 className='text-primary yaoti'>我們相信 <br/>每個孩子都有自己選擇職涯的權利</h1>
-                    <div className="d-flex py-3">
-                        <div className="d-flex align-items-center left">
-                            <blockquote className="d-block quote">我們是臺灣的教育團隊——直覺職掘，「透過舉辦職業體驗營，讓國中生的選擇更自由。」
-直覺×職掘從二年前開始，在寒暑假期間，為國中生舉辦職涯探索的工作坊， 邀請各個專業領域的講師，為國中生講解各項職業技能，搭配實作體驗， 讓學員從做中學習，在學科外的領域得到成就感，重新激發學習動機。</blockquote>
+                    <div className="py-3 row">
+                        <div className="d-flex align-items-center col-md-6">
+                            <blockquote className="d-block quote">我們是臺灣的教育團隊——直覺職掘，「透過舉辦職業體驗營，讓國中生的選擇更自由。」 直覺×職掘從二年前開始，在寒暑假期間，為國中生舉辦職涯探索的工作坊， 邀請各個專業領域的講師，為國中生講解各項職業技能，搭配實作體驗， 讓學員從做中學習，在學科外的領域得到成就感，重新激發學習動機。</blockquote>
                         </div>
-                        <div className="right">
+                        <div className="col-md-6">
                             <img src={img1} alt='' width="100%" />
                         </div>
                     </div>
                 </div>
-                <div className="text-center text-container">
-                    <h1 className="yaoti">我們認為孩子有自己選擇的權力</h1>
+                <div className="small-container text-center text-container my-5">
+                    <h1 className="yaoti text-primary">實作體驗，創造成就</h1>
+                		<div className="row py-3">
+                        <div className="col-md-6">
+                            <img src={img1} alt='' width="100%" />
+                        </div>
+                        <div className="d-flex align-items-center col-md-6">
+                            <blockquote className="d-block quote">為了讓孩子們有更深刻的學習，我們在三小時的課程中， 安排一個半小時至兩小時的體驗實作時間，讓孩子們能跳脫傳統教學方式由上而下的框架， 親自動手參與課程，遇到問題便想辦法解決，學習專業知識的過程亦培養解決問題的能力。</blockquote>
+                        </div>
+                    </div>
                 </div>
 
             </div>
